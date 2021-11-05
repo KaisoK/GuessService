6a9964cd3ed8b29da836f0088342c62f975db01f
KaisoK <92855871+KaisoK@users.noreply.github.com>
2021-11-05 12:30:57 +0000

    Add files via upload

---
+++ GuessServiceV1.2.py
@@ -0,0 +1,47 @@
+import socks
+import socket
+import subprocess
+
+x = open("/home/kaisok/Documentos/Python/Onion/Lista", "r", encoding="latin-1")
+url = x.readlines()
+x.close()
+subprocess.run("clear")
+
+
+for i in url:
+    nombre = i.strip("\n")
+    i = i.split(" ")[1].strip("\n")
+
+for puerto in range(1000, 3000):
+
+    try:
+        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9050, True)
+        s = socks.socksocket()
+        s.connect(('{}', '{}'.format(puerto)))
+        a = s.recv(1024).decode()
+
+        if "SSH" in a:
+            print("\n#######################")
+            print("\n" + nombre)
+            print("\nEs SSH")
+            print("\n#######################")
+            
+        elif "FTP" in a:
+            print("\n#######################")
+            print("\n" + nombre)
+            print("\nEs FTP")
+            print("\n#######################")
+            
+        else:
+            print("\n#######################")
+            print("\n" + nombre)
+            print("Otro servicio")
+            print("\n#######################")
+        
+        s.close()
+        
+    except:
+        print("\n#######################")
+        print("\n{}\n\nHost inaccesible".format(nombre))
+        print("\n#######################")
+        
